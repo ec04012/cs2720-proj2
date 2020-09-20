2544a58731ba46b9682a2c1fc7912f48d651b2aa
ec04012 <ec04012@uga.edu>
2020-09-20 16:10:20 -0400

    Added Student files

---
+++ Student.cpp
@@ -0,0 +1,43 @@
+#include "Student.h"
+
+// Class constructors
+// Default constructor, uses placeholder name and id
+Student::Student() {
+    this->setID(1111);
+    this->setName("John Smith");
+}
+// Param constructor, gets name and id as param
+Student::Student(int const& id, string const& name) {
+    this->setID(id);
+    this->setName(name);
+}
+
+// get methods
+int Student::getID() const {
+    return id;
+}
+string Student::getName() const {
+    return name;
+}
+
+// set methods
+void Student::setID(int const& newID) {
+    this->id = newID;
+}
+void Student::setName(string const& newName) {
+    this->name = newName;
+}
+
+// Operator overloads
+bool Student::operator==(Student& s) {
+    if (id == s.getID()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+ostream & operator<<(ostream &out, const Student&stu) {
+    out << stu.getID() << " " << stu.getName();
+    return out;
+}
---
+++ Student.h
@@ -0,0 +1,34 @@
+#ifndef STUDENT
+#define STUDENT
+
+#include <iostream>
+#include <ostream>
+#include <string>
+using namespace std;
+
+class Student {
+   public:
+    // Class constructors
+    Student();                             // Default constructor, uses placeholder name and id
+    Student(int const &, string const &);  // Param constructor, gets name and id as param
+
+    // No destructor, the default destructor should work
+
+    // get methods
+    int getID() const;
+    string getName() const;
+
+    // set methods
+    void setID(int const &);
+    void setName(string const &);
+
+    // Operator overloads
+    bool operator==(Student &s);
+    friend ostream &operator<<(ostream &out, const Student &stu);
+
+   private:
+    int id;
+    string name;
+};
+
+#endif
